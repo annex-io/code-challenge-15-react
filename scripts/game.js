5bef6057125a50bcf3e1ab64b1ec3ca36a3ea575
annex <lawry.xerri@gmail.com>
2018-02-09 15:00:12 +1100

    add init function and variable scope; win funciton; code cleanup; comments;

--- scripts/game.js
+++ scripts/game.js
@@ -1,17 +1,40 @@
-const boardSize = 16;
-const boardElem = '.board';
-const emptyTile = "&nbsp;";
-
+/*
+ * Assumptions: 
+ * - Duplicate numbers are ok for now
+ * - It's unlikely that a player will win without first making a move
+ * 
+ * TODO: 
+ * - Slide rule was not implemented. Could add in a check to only allow a click on tiles next to the empty tile. 
+ * - 
+ *
+*/
 var game = {  
 	
 	spaces: {},
+
+	settings: {
+		boardSize : 16,
+		boardElem : '.board',
+		emptyTile : "&nbsp;"
+	},
 	
+	init: function() {
+		// init spaces & tiles
+		for (var i = 1; i < this.settings.boardSize; i++ ) {
+			game.spaces[i] = Math.floor(Math.random() * (this.settings.boardSize - 1)) + 1; 
+		}
+		game.spaces[this.settings.boardSize] = this.settings.emptyTile;
+
+		game.draw();
+	},
+
 	draw: function() {
 
-		$(boardElem).empty();
+		$(this.settings.boardElem).empty();
 
-		var $board = $(boardElem);
+		var $board = $(this.settings.boardElem);
 		
+		// draw dynamic elems and bind events based on object
 		for(var key in this.spaces) { 
 			var $tile = $('<div class="space"></div>');
 			
@@ -19,45 +42,56 @@ var game = {
 				.attr('data-space', key)
 				.attr('data-tile', this.spaces[key])
 				.html(this.spaces[key])
-
-				// todo: allow only the tiles net to the space to be clickable...		
 				.click(function() {	
 					var space = $(this).attr('data-space');
-					console.log('space clicked: ' + space);
 					
 					game.move(space);
 					game.draw();
+					game.win();
 				});
 	
 			$board.append($tile);
 		}
 
-		$(boardElem).append($board);		
+		$(this.settings.boardElem).append($board);		
 	},
 
 	move: function(space) {
 		var tileVal = this.spaces[space];
-		console.log('tile at this space ' + tileVal);
 
- 		// find the empty space to add it
+ 		// find the empty space to move tile there
  		for(var key in this.spaces) {
- 			if (this.spaces[key] === emptyTile) {
- 				this.spaces[key] = tileVal;
- 				console.log('the empty space found was ' + key + ' and we added tile ' + this.spaces[key])
+ 			if (this.spaces[key] === this.settings.emptyTile) {
+ 				this.spaces[key] = tileVal; 				
  			}
  		} 		
 
- 		// clear clicked space
- 		console.log('tile as prev ' + this.spaces[space]);
-		this.spaces[space] = emptyTile;
-		console.log('tile as curr ' + this.spaces[space]);
+ 		// clear clicked space 
+		this.spaces[space] = this.settings.emptyTile;		
 	},
 
-	isWin: function() {
-		// check data to see if the player has won
-	}
-}
+	win: function() {
+		var clone = Object.assign({}, this.spaces);
+		var prev = 0;
+		var win = true;
 
-function handler() {
-	console.log(handler);
+		// delete empty tile from object so we can test
+		for (var key in clone) if (clone[key] == this.settings.emptyTile) delete clone[key]
+
+		// note: maybe a better way to sort and compare...
+		for (var key in clone) {
+			
+			if (prev > clone[key]) {
+				win = false;
+				break;
+			}
+
+			prev = clone[key];
+		}
+
+		if (win) {
+			alert("You are a winner!");
+			$(this.settings.boardElem).css('border', 'solid 5px #56d1e3');
+		}
+	}
 }
