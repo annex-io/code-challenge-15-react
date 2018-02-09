8633fc50380618005a81161d885726e2c6291363
annex <lawry.xerri@gmail.com>
2018-02-09 13:42:44 +1100

    click event on dynamic elements;

--- scripts/game.js
+++ scripts/game.js
@@ -1,5 +1,6 @@
 const boardSize = 16;
-
+const boardElem = '.board';
+const emptyTile = "&nbsp;";
 
 var game = {  
 	
@@ -7,25 +8,56 @@ var game = {
 	
 	draw: function() {
 
-		var tiles = "";
+		$(boardElem).empty();
+
+		var $board = $(boardElem);
 		
-		for(var key in this.spaces) 
-			tiles += "<div class='space space-"+ key +" tile- "+ this.spaces[key] +"'>"+ this.spaces[key] +"</div>";
-				
-		$('.board').append(tiles);
+		for(var key in this.spaces) { 
+			var $tile = $('<div class="space"></div>');
+			
+			$tile
+				.attr('data-space', key)
+				.attr('data-tile', this.spaces[key])
+				.html(this.spaces[key])
+
+				// todo: allow only the tiles net to the space to be clickable...		
+				.click(function() {	
+					var space = $(this).attr('data-space');
+					console.log('space clicked: ' + space);
+					
+					game.move(space);
+					game.draw();
+				});
+	
+			$board.append($tile);
+		}
+
+		$(boardElem).append($board);		
 	},
 
 	move: function(space) {
-		console.log(this.spaces[space]);
-		
-		// move val 		
- 		var moveVal = this.spaces[space];
+		var tileVal = this.spaces[space];
+		console.log('tile at this space ' + tileVal);
 
  		// find the empty space to add it
- 		for(var key in this.spaces)
-			if (!this.spaces[key].length) this.spaces[key] = moveVal;
- 		
+ 		for(var key in this.spaces) {
+ 			if (this.spaces[key] === emptyTile) {
+ 				this.spaces[key] = tileVal;
+ 				console.log('the empty space found was ' + key + ' and we added tile ' + this.spaces[key])
+ 			}
+ 		} 		
+
  		// clear clicked space
-		this.spaces[space] = "";
+ 		console.log('tile as prev ' + this.spaces[space]);
+		this.spaces[space] = emptyTile;
+		console.log('tile as curr ' + this.spaces[space]);
+	},
+
+	isWin: function() {
+		// check data to see if the player has won
 	}
+}
+
+function handler() {
+	console.log(handler);
 }
